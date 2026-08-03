60d3bf0da93aeeeb6225640637aac765b0bc6c93
Gloire Bikouta <134594230+gloiiire@users.noreply.github.com>
2026-08-03 00:16:52 +0000

    chore(bottle): refresh sha256 for v1.0.1.0

--- Formula/cocker.rb
+++ Formula/cocker.rb
@@ -28,7 +28,7 @@ class Cocker < Formula
   # tag so they stay in lock-step.
   bottle do
     root_url "https://github.com/gloiiire/cocker/releases/download/v1.0.1.0"
-    sha256 cellar: :any_skip_relocation, all:      "REPLACE_BOTTLE_SHA256_ALL"
+    sha256 cellar: :any_skip_relocation, all:      "36246c84b3b0c3419c2cefda83d488f1685a640fd6bc5933dd20d54378aa5174"
   end
 
   depends_on arch: :arm64
